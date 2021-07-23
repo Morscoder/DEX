616638e1b89ff8cd94f72e0ad7e0aed73b5e364d
Ahmed <a.morsalin@gmail.com>
2021-07-23 02:08:06 +0200

    initial commit

---
+++ client/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">
+    <a style={{color: 'white'}}> email: a.morsalin@gmail.com</a>     & I'm the developer you need.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
